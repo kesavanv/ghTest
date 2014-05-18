fae4753923e01a6bef3da9c86738164db7e85e53
Varatharajan, Kesavan <kvaratharajan@paypal.com>
2014-05-18 20:34:57 +0530

    Adding Google map for singapore location

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,25 @@
 $(function() {
     $(".secondaryModalTrigger").click(function(e) {
-    	var currentModal = $(e.target).data('self');
-    	$(currentModal).modal('hide');
+		var currentModal = $(e.target).data('self');
+		$(currentModal).modal('hide');
     });
+
+    function initMap() {
+      var singapore = new google.maps.LatLng(1.336445,103.700715);
+      var mapOptions = {
+        zoom: 17,
+        center: singapore
+      };
+
+      var map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
+
+      var marker = new google.maps.Marker({
+          position: singapore,
+          map: map,
+          title: 'GTEC - Singapore'
+      });
+    }
+
+
+  	google.maps.event.addDomListener(window, 'load', initMap);
 });
