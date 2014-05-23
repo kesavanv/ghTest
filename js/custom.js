d9a5aa01b00d003c90ad7d340e231e6fbdfcc171
Varatharajan, Kesavan <kvaratharajan@paypal.com>
2014-05-24 00:19:15 +0530

    updated rev 4

--- js/custom.js
+++ js/custom.js
@@ -10,22 +10,69 @@ $(function() {
 		});
 	});
 
-    function initMap() {
-      var singapore = new google.maps.LatLng(1.336445,103.700715);
-      var mapOptions = {
-        zoom: 17,
-        center: singapore
-      };
+function initialize() {
+  var mapOptions = {
+    zoom: 2,
+    center: new google.maps.LatLng(48.0658107,-3.567693)
+  }
+  var map = new google.maps.Map(document.getElementById('map_canvas'),
+                                mapOptions);
 
-      var map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
+  setMarkers(map, beaches);
+}
 
-      var marker = new google.maps.Marker({
-          position: singapore,
-          map: map,
-          title: 'GTEC - Singapore'
-      });
-    }
+/**
+ * Data for the markers consisting of a name, a LatLng and a zIndex for
+ * the order in which these markers should display on top of each
+ * other.
+ */
+var beaches = [
+  ['UK', 51.7745899,-0.0102354, 4],
+  ['USA', 37.761253,-121.902994, 3],
+  ['India', 12.950816,80.140335, 2],
+  ['Singapore', 1.3185848,103.8455665, 1]
+];
 
+function setMarkers(map, locations) {
+  // Add markers to the map
 
-  	google.maps.event.addDomListener(window, 'load', initMap);
+  // Marker sizes are expressed as a Size of X,Y
+  // where the origin of the image (0,0) is located
+  // in the top left of the image.
+
+  // Origins, anchor positions and coordinates of the marker
+  // increase in the X direction to the right and in
+  // the Y direction down.
+  var image = {
+    url: 'images/flags.png',
+    // This marker is 20 pixels wide by 32 pixels tall.
+    size: new google.maps.Size(20, 32),
+    // The origin for this image is 0,0.
+    origin: new google.maps.Point(0,0),
+    // The anchor for this image is the base of the flagpole at 0,32.
+    anchor: new google.maps.Point(0, 32)
+  };
+  // Shapes define the clickable region of the icon.
+  // The type defines an HTML &lt;area&gt; element 'poly' which
+  // traces out a polygon as a series of X,Y points. The final
+  // coordinate closes the poly by connecting to the first
+  // coordinate.
+  var shape = {
+      coords: [1, 1, 1, 20, 18, 20, 18 , 1],
+      type: 'poly'
+  };
+  for (var i = 0; i < locations.length; i++) {
+    var beach = locations[i];
+    var myLatLng = new google.maps.LatLng(beach[1], beach[2]);
+    var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        shape: shape,
+        title: beach[0],
+        zIndex: beach[3]
+    });
+  }
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
 });
